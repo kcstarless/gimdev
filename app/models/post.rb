3afa1dd6dcf736ad2e51510c9282f483127eebf2
kcstarless <155853062+kcstarless@users.noreply.github.com>
2025-01-14 10:42:35 +1100

    uploading to github

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
-  has_and_belongs_to_many :categories, dependent: :destroy
+  has_and_belongs_to_many :categories
   has_and_belongs_to_many :keywords
   has_many :sections, dependent: :destroy
-  has_one_attached :cover_image
+  has_one_attached :cover_image, dependent: :destroy
 
   accepts_nested_attributes_for :sections, allow_destroy: true, reject_if: :all_blank
 
